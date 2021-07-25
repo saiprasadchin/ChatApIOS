3a03e7f5d6fa44a54c90c14b862e4c8e69444b04
saiprasadchindamchin <saiprasad@chindam.com>
2021-07-25 17:22:39 +0530

    Initial Commit

---
+++ ChatAppIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatAppIOS
+//
+//  Created by saiprasad chindam on 25/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
